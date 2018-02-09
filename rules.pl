d9777995c4e0508dae9d3436fcfa780c5b3e82be
Tom Powell <tom@powell.io>
2018-02-08 21:27:09 -0800

    test prolog rules
    Change-Id: Ib3a3a45d68ca9d769aa18298af09f5b24e057175

---
+++ rules.pl
@@ -0,0 +1,20 @@
+submit_rule(submit(CR)) :-
+    sum(6, 'Code-Review', CR).
+
+% Sum the votes in a category. Uses a helper function score/2
+% to select out only the score values the given category.
+sum(VotesNeeded, Category, label(Category, ok(_))) :-
+    findall(Score, score(Category, Score), All),
+    sum_list(All, Sum),
+    Sum >= VotesNeeded,
+    !.
+sum(VotesNeeded, Category, label(Category, need(VotesNeeded))).
+
+score(Category, Score) :-
+    gerrit:commit_label(label(Category, Score), User).
+
+% Simple Prolog routine to sum a list of integers.
+sum_list(List, Sum)   :- sum_list(List, 0, Sum).
+sum_list([X|T], Y, S) :- Z is X + Y, sum_list(T, Z, S).
+sum_list([], S, S).
+
