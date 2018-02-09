277a24395d207b89b99edcd8a4d8633a0954387c
Tom Powell <tom@powell.io>
2018-02-08 21:40:48 -0800

    temp set to 4
    Change-Id: I9c1685aca0502c491d2a2174d5b7a4b5990602c5

--- rules.pl
+++ rules.pl
@@ -1,5 +1,5 @@
 submit_rule(submit(CR)) :-
-    sum(6, 'Code-Review', CR).
+    sum(4, 'Code-Review', CR).
 
 % Sum the votes in a category. Uses a helper function score/2
 % to select out only the score values the given category.
@@ -17,4 +17,3 @@ score(Category, Score) :-
 sum_list(List, Sum)   :- sum_list(List, 0, Sum).
 sum_list([X|T], Y, S) :- Z is X + Y, sum_list(T, Z, S).
 sum_list([], S, S).
-
